7c66014e2014d09b05e9302df203a1db47688751
RAPHAEL ANTONIO DA COSTA <54811083+RaphaelAntoniio@users.noreply.github.com>
2023-02-22 20:40:18 -0300

    Add files via upload

---
+++ Game1/assents/js/script.js
@@ -0,0 +1,30 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () =>{
+    mario.classList.add('jump');
+    setTimeout(()=>{
+        mario.classList.remove('jump');
+    },500)
+}
+
+const loop = setInterval(()=>{
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px','');
+
+    if(pipePosition <= 41 && pipePosition > 0 && marioPosition < 100){
+        pipe.style.animation='none';
+        pipe.style.left=`${pipePosition}px`
+        mario.style.animation='none';
+        mario.style.bottom=`${pipePosition}px`
+        mario.src = './assents/images/mario-game-over.gif';
+        mario.style.width= '75px';
+        mario.style.margin.left='50px';
+        
+        clearInterval(loop);
+    }
+
+} ,10)
+
+document.addEventListener('keydown', jump);
